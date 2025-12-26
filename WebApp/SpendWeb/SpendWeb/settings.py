facdfefeb80b3435ae0dd260d167b661d2b476c7
denzalzc <deniszabolzc@gmail.com>
2025-12-26 15:02:47 +0400

    i

--- WebApp/SpendWeb/SpendWeb/settings.py
+++ WebApp/SpendWeb/SpendWeb/settings.py
@@ -25,9 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nl=we4wa!vvqe@cj&@c$1%s5fl==&*nk&lk9kmu4a5s$!q9-&^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '80.93.61.179',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
--- up_server.bash
+++ up_server.bash
@@ -1,8 +1,13 @@
-cp nginx.art /etc/nginx/sites-available/codesworld
+cp nginx.art /etc/nginx/sites-available/spendingBot
 nginx -t
 systemctl reload nginx
 service nginx reload
-sudo ln -s /etc/nginx/sites-available/codesworld /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/spendingBot /etc/nginx/sites-enabled/
 .././venv/bin/python3 manage.py collectstatic
 DJANGO_DEBUG=False
-.././venv/bin/python3 .././venv/bin/gunicorn --bind 127.0.0.1 codesworld.wsgi:application
+
+systemctl start spendbot
+systemctl enable spendbot
+
+systemctl start spendweb
+systemctl enable spendweb
