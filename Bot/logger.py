60beac1a692ed509a57e3ff9f4bb61c63c0e139b
denzalzc <deniszabolzc@gmail.com>
2025-12-30 19:49:24 +0400

    1. add logger

--- Bot/logger.py
+++ Bot/logger.py
@@ -9,6 +9,6 @@ class Logger:
     def log(self, write_type: str, utgid: int, userid: int, add_text: str = ''):
         with open(self.logfilepath, 'a') as f:
             f.write(
-                f'[{datetime.now().strftime('%d/%m/%Y|%H:%M:%S')}] [DB_id:{userid} TG_id:{utgid}] [{write_type}] info:[{add_text}]'
+                f'[{datetime.now().strftime('%d/%m/%Y|%H:%M:%S')}] [DB_id:{userid} TG_id:{utgid}] [{write_type}] info:[{add_text}]\n'
             )
 
